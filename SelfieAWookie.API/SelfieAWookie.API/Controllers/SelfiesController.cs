032b4fc4bf62d3806960084dddad7d24c31842bf
Pierre Moreno <clad66@hotmail.com>
2022-08-21 13:44:21 +0200

    Mise en place de la base

---
+++ SelfieAWookie.API/SelfieAWookie.API/Controllers/SelfiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfieAWookie.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SelfiesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Selfie> Get()
+        {
+            return Enumerable.Range(1, 10).Select(item => new Selfie() { Id = item });
+        }
+    }
+}
---
+++ SelfieAWookie.API/TestWebAPI/SelfieControllerUnitTest.cs
@@ -0,0 +1,24 @@
+using SelfieAWookie.API.Controllers;
+using Xunit;
+
+namespace TestWebAPI
+{
+    public class SelfieControllerUnitTest
+    {
+        [Fact]
+        public void ShouldReturnListOfSelfies()
+        {
+            // ARRANGE
+            var controller = new SelfiesController();
+
+            // ACT
+            var result = controller.Get();
+
+            // ASSERT
+            Assert.NotNull(result); 
+            Assert.True(result.Count() > 0);    
+
+
+        }
+    }
+}
